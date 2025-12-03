ddc3ebaf97924e997eb6b4cafbceace9bc692d9f
stevendcwork <stevendelacruz.work@gmail.com>
2025-12-03 16:30:32 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold">Steve Dela Cruz</h1>
+      <p className="mt-4 text-lg">
+        Freelance web developer, IT support specialist and admin work
+      </p>
+    </div>
+  )
+}
